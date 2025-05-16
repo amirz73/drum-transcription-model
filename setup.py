2dab5ef8b543e2dc80e14bfff0df72ce649d841f
amirz73 <88727584+amirz73@users.noreply.github.com>
2025-05-16 13:32:41 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# drum-transcription-model/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="drum_model",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # e.g. "numpy>=1.24.0", "torch>=2.0.0", etc.
+    ],
+)
